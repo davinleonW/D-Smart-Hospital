10c26297581eba8802af408eaec19555a52ca6f2
davinleonW <davinleon.w@gmail.com>
2026-07-07 13:51:23 +0700

    Update hospital_app.py

--- hospital_app.py
+++ hospital_app.py
@@ -2,6 +2,6 @@ import streamlit as stt
 import pandas as pd
 import pickle
 
-with open("hospital_model.pkl","rb")as f
+with open("Davin-hospital_model.pkl","rb")as f
 bundle = pickle.load(f)
 st.write("Connected")
